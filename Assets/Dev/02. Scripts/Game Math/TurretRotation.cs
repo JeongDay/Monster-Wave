278b6f69da8135756f1cadb4410c51fbc2ce082d
JeongDay <cidd1332@gmail.com>
2025-02-18 21:03:15 +0900

    [게임 수학] 삼각함수 그래프, 라이트, 터렛 회전

---
+++ Assets/Dev/02. Scripts/Game Math/TurretRotation.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class TurretRotation : MonoBehaviour
+{
+    public float rotationSpeed = 3f;
+
+    private float theta;
+    
+    void Update()
+    {
+        // this.transform.Rotate(Vector3.up * Time.deltaTime * rotationSpeed);
+        
+        theta += Time.deltaTime * rotationSpeed;
+        this.transform.localRotation = Quaternion.Euler(Vector3.up * 45f * Mathf.Sin(theta));
+
+        Vector3 vecA = new Vector3(0, 3, 5);
+        Vector3 vecB = new Vector3(7, 2, 1);
+
+        float result1 = Vector3.Magnitude(vecB - vecA);
+        float result2 = Vector3.Distance(vecB, vecA);
+    }
+}
